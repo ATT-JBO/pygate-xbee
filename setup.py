dc8a053ba6902935ea830f78b207e38c949077bd
Jan Bogaerts <jb@allthingstalk.com>
2016-09-12 18:24:16 +0200

    init checkin

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='pygate_xbee',
+    version='1.0',
+    packages=['pygate_xbee'],
+    url='www.allthingstalk.com',
+    license='',
+    author='Jan Bogaerts',
+    author_email='jb@allthingstalk.com',
+    description="plugin for the pygate: connect devices that communicate with the xbee protocol"
+)
